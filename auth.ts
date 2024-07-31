523a7f94e8e399ebf55df2e3b95181bb4192c568
andrewnguyen-nsw <andrewnguyen.nsw@gmail.com>
2024-07-31 21:39:29 +1000

    Installed Shadcn

--- auth.ts
+++ auth.ts
@@ -1,5 +1,27 @@
-import NextAuth from "next-auth"
- 
+import NextAuth from "next-auth";
+import Credentials from "next-auth/providers/credentials";
+
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [],
-})
+  providers: [
+    Credentials({
+      credentials: {
+        email: {},
+        password: {},
+      },
+      authorize: async (credentials) => {
+        let user = null;
+
+        // Logic to hash password with bcrypt
+
+        // Logic to verify if the user exists
+
+        if (!user) {
+          throw new Error("User not found");
+        }
+
+        // Return user object
+        return user;
+      },
+    }),
+  ],
+});
